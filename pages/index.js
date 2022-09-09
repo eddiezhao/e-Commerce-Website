128c638c1248eb9a6581749b5400bb0a03500875
eddie <eddiezhao@DESKTOP-RJKILPO>
2022-09-09 00:13:00 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Product, FooterBanner, HeroBanner } from '../components'
+
+const Home = () => {
+  return (
+    <div>
+      <HeroBanner />
+      <div className="products-heading">
+        <h2>
+          Best Selling Products
+        </h2>
+        <p>
+          Banner
+        </p>
+      </div>
+      <div className="products-container">
+        {['Product1', 'Product2'].map(
+          (product) => product)}
+      </div>
+
+      <FooterBanner />
+    </div>
+  )
+}
+
+export default Home
